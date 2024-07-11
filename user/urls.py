0e0cb425680fe87d861700ab5fa6497505510ce0
VARMA SHAILESH KUMAR <87381420+shaileshkumar001@users.noreply.github.com>
2024-07-10 23:29:53 -0700

    Add files via upload

---
+++ user/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('index/',views.index),
+    path('',views.index),
+    path('about/',views.about),
+    path('contact/',views.contact),
+    path('signin/',views.signin),
+    path('signup/',views.signup),
+    path('event/',views.myevent),
+    path('imagegallery/',views.igallery),
+    path('videogallery/',views.vgallery),
+    path('viewdetails/',views.viewdetails),
+    path('logout/',views.logout),
+
+]
+
